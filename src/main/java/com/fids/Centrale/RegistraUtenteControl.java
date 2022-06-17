3cfa26283a2081bb0d0b9cb8b1fe32d94ecbf19b
Riccardo Sciacca <riccardo.sciacca@community.unipa.it>
2022-06-17 19:08:24 +0200

    17/06 19:08

--- src/main/java/com/fids/Centrale/RegistraUtenteControl.java
+++ src/main/java/com/fids/Centrale/RegistraUtenteControl.java
@@ -1,19 +1,15 @@
 package com.fids.Centrale;
 
 import DBMSB.DBMSBoundary;
-import javafx.beans.property.SimpleStringProperty;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.paint.Color;
-import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.sql.*;
 import java.time.format.DateTimeFormatter;
+import java.util.*;
 
 public class RegistraUtenteControl {
     @FXML
@@ -34,16 +30,17 @@ public class RegistraUtenteControl {
     public Label erroreLabel;
     @FXML
     private Button confermaRegistraButton;
+    HashMap<String, String> utentiMap = new HashMap<String, String>();
     private DBMSBoundary dbms = new DBMSBoundary();
     ResultSet utenti= dbms.getUtenti();
 
     public void setFields(){
         String idSede="-1";
         try{
-            while(utenti.next()){
-                if(!utenti.getString("IDSede").equals(idSede)){
+            while(utenti.next()) {
+                if (!utenti.getString("IDSede").equals(idSede)) {
                     sedeField.getItems().add(utenti.getString("nomeSede"));
-                    idSede=utenti.getString("IDSede");
+                    idSede = utenti.getString("IDSede");
                 }
             }
         }catch (Exception e) {
@@ -52,6 +49,7 @@ public class RegistraUtenteControl {
         ruoloField.getItems().add("Farmacista");
         ruoloField.getItems().add("Impiegato");
         ruoloField.getItems().add("Corriere");
+        System.out.println(utentiMap);
     }
 
     public void ruoloFarmacista(ActionEvent event) throws IOException {
@@ -65,37 +63,45 @@ public class RegistraUtenteControl {
         }
     }
     public void registraUtente(ActionEvent event) throws IOException{
-        String nome = nomeField.getText();
-        String cognome = cognomeField.getText();
-        String email = emailField.getText();
+        String nome = nomeField.getText().toLowerCase();
+        String cognome = cognomeField.getText().toLowerCase();
+        String email = emailField.getText().toLowerCase();
         String dataNascita=null;
-        if(dataNascitaField.getValue()!=null) {
+        if(dataNascitaField.getValue()!=null)
             dataNascita = dataNascitaField.getValue().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));//dataNascitaField.getValue().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
-        }
         String ruolo = ruoloField.getValue();
         String sede = sedeField.getValue();
-
+        String IDSede = String.valueOf(sedeField.getSelectionModel().getSelectedIndex()+1);
 
         if(nome.trim().isEmpty() || cognome.trim().isEmpty() || email.trim().isEmpty() || dataNascita==null || ruolo==null || (ruolo=="Farmacista" && sede==null)){
             erroreLabel.setText("Tutti i campi sono obbligatori!");
             erroreLabel.setTextFill(Color.color(1, 0, 0));
         } else{
-            System.err.println("nome:"+nome+nome.trim().isEmpty()+"\ncognome:"+cognome+cognome.trim().isEmpty()+"\nemail:"+email+email.trim().isEmpty()+"\ndataNascita:"+dataNascita+dataNascita.trim().isEmpty()+"\nruolo:"+ruolo+"\nsede:"+sede);
-            try{
-                //utenti.first();
-                while(utenti.next()){
-                    System.err.println(utenti.getString("email"));
-                    if(utenti.getString("email").equals(email)){
-                        erroreLabel.setText("L'utente inserito è già stato registrato!");
-                        erroreLabel.setTextFill(Color.color(1, 0, 0));
-                        break;
+            if(dbms.verificaEmail(email)) {
+                erroreLabel.setText("L'utente inserito è già stato registrato!");
+                erroreLabel.setTextFill(Color.color(1, 0, 0));
+                System.err.println("Utente già inserito");
+            }else{
+                ArrayList usernames = new ArrayList<String>();
+                usernames=dbms.getUsernames();
+                String username=nome+"."+cognome;
+                int num=1;
+                boolean flag=true;
+                while(flag) {
+                    flag=true;
+                    for (String u : usernames) {
+                        if (username.equals(u)){
+                            username=nome+"."+cognome+String.valueOf(num);
+                            num++;
+                            flag=false;
+                        }
                     }
                 }
-            }catch (Exception e) {
-                e.printStackTrace();
+                dbms.insertUtente(nome, cognome, dataNascita, email, username, ruolo, IDSede);
+                erroreLabel.setText("Aggiunto!");
+                erroreLabel.setTextFill(Color.color(1, 0, 0));
             }
         }
     }
-
 
 }
