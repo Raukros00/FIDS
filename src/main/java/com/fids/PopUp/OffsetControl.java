55e8fce305dfe75ae46fb70f4a01ea5fdda6407d
Riccardo Sciacca <riccardo.sciacca@community.unipa.it>
2022-06-20 15:07:02 +0200

    20/06 15:06

---
+++ src/main/java/com/fids/PopUp/OffsetControl.java
@@ -0,0 +1,18 @@
+package com.fids.PopUp;
+
+import javafx.scene.control.Spinner;
+import javafx.scene.control.TextField;
+
+import javax.xml.datatype.DatatypeConstants;
+
+public class OffsetControl {
+    private Spinner<Integer> offsetOraField;
+    /*
+    private Spinner<Integer> offsetGiorniField;
+    public void impostaOffset(){
+        int offsetOra=offsetOraField.getValue();
+        int offsetGiorni=offsetGiorniField.getValue();
+
+
+    }*/
+}
